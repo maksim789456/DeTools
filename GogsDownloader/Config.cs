91196a67fb1c4ff1a3bda37c2ac73f4caca29aa2
maksim789456 <maksim_789456@mail.ru>
2022-10-17 01:16:54 +0400

    Gogs database type

--- GogsDownloader/Config.cs
+++ GogsDownloader/Config.cs
@@ -4,6 +4,7 @@ public class Config
 {
     public string BaseGogsUrl { get; set; } = "http://localhost:3000";
     public string ConnectionString { get; set; } = "";
+    public DatabaseType DatabaseType { get; set; } = DatabaseType.MySql;
     public User[] Users { get; set; } = Array.Empty<User>();
 }
 
@@ -11,4 +12,11 @@ public class User
 {
     public string Username { get; set; }
     public string Password { get; set; }
+}
+
+public enum DatabaseType
+{
+    Postgre,
+    MySql,
+    Sqlite
 }
